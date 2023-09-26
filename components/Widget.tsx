36f84a7822b9e3a65e6b6c36ec837c88ce192e4f
MemoiVentur3 <141185642+MemoiVentur3@users.noreply.github.com>
2023-09-26 09:14:21 +0000

    update

--- components/Widget.tsx
+++ components/Widget.tsx
@@ -1,19 +1,19 @@
 import { WidgetEvents } from './WidgetEvents';
 import { LiFiWidget, WidgetConfig } from '@lifi/widget';
 import { useMemo } from 'react';
-import Navbar from './Navbar';
 
 export const Widget = () => {
   const widgetConfig: WidgetConfig = useMemo(() => ({
     containerStyle: {
+      variant: 'nft',
       marginTop: '35px',
-      boxShadow: '0px 8px 32px rgba(0, 0, 0, 0.08)',
+      boxShadow: '0px 8px 32px rgba(0, 0, 0, 1.08)',
       borderRadius: '16px',
     },
     theme: {
       palette: {
         primary: { main: '#2196F3' },
-        secondary: { main: '#F5B5F' },
+        secondary: { main: '#2196F' },
       },
       shape: {
         borderRadius: 20,
@@ -31,7 +31,6 @@ export const Widget = () => {
 
   return (
     <>
-      <Navbar />
       <WidgetEvents />
       <LiFiWidget
         config={widgetConfig}
